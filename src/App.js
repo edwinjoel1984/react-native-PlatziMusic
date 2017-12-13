587601e1f72b6f9a091673206f43306b6df0de47
Edwin Ulloa <edwinjoel1984@gmail.com>
2017-12-13 17:30:25 -0500

    Add Facebook in Androids Devices

--- src/App.js
+++ src/App.js
@@ -19,13 +19,13 @@ import ArtistDetailView from './ArtistDetailView.js';
 
 export default class App extends Component<{}> {
   render() {
-    const isAndroid= Platform.OS !== 'android';
+    const isAndroid= Platform.OS === 'android';
     return <Router>
-      <Scene key="root">
-        <Scene key="login" component={LoginView} hideNavBar={isAndroid}  />
+      <Scene key="root" hideNavBar={isAndroid}> 
+        <Scene key="login" component={LoginView}   />
         <Scene key="secundary">
           <Scene key="home" component={HomeView} hideNavBar={isAndroid} />
-          <Scene key="artistDetail" component={ArtistDetailView} hideNavBar={isAndroid}/>
+          <Scene key="artistDetail" component={ArtistDetailView} hideNavBar={false} />
       </Scene>
       </Scene>
       
